e84d416608bf8c7833b13ebc4ef72827dd7ba7a2
Anton Mamkin <mail@tbrd.ru>
2019-07-30 02:41:59 +0300

    first

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
@@ -0,0 +1,24 @@
+package ru.skillbranch.devintensive.models
+
+data class Profile(
+    val firstName: String,
+    val lastName: String,
+    val about: String,
+    val repository: String,
+    val rating: Int = 0,
+    val respect: Int = 0
+) {
+    val nicName: String = "John Doe"
+    val rank: String = "Junior Android Developer"
+
+    fun toMap(): Map<String, Any> = mapOf(
+        "nicName" to nicName,
+        "rank" to rank,
+        "firstName" to firstName,
+        "lastName" to lastName,
+        "about" to about,
+        "repository" to repository,
+        "rating" to rating,
+        "respect" to respect
+    )
+}
